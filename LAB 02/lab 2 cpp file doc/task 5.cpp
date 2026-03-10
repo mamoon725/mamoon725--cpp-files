a91d0adc61043fbb156143823408c459ea35f8b1
mamoon725 <khattakmamoon725@gmail.com>
2026-03-10 09:58:15 -0700

    RENAME

---
+++ LAB 02/lab 2 cpp file doc/task 5.cpp
@@ -0,0 +1,24 @@
+
+//Sequential 
+//print your name 10 times 
+
+#include <iostream>// This line lets us use input cin and output cout in the program
+using namespace std;// Use the standard namespace to avoid std :: before  every cout
+
+int main()
+	{
+	
+	
+	cout<<" MAMOON "<<endl; // Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<" MAMOON"<<endl;// Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<" MAMOON "<<endl;// Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<" MAMOON "<<endl; // Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<" MAMOON  "<<endl;
+	cout<<" MAMOON "<<endl;// Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<" MAMOON "<<endl;
+	cout<<" MAMOON "<<endl;
+	cout<<" MAMOON"<<endl;// Print my name 10 times and using endl with my name will end one line and creats a new line in the output
+	cout<<" MAMOON "<<endl;	
+		
+	return 0;}// program end successfuly
+
