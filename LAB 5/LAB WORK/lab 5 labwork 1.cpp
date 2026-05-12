ef143074aab0a8db162eec339450f25f114cb094
mamoon725 <khattakmamoon725@gmail.com>
2026-05-12 07:27:46 -0700

    Add files via upload

---
+++ LAB 5/LAB WORK/lab 5 labwork 1.cpp
@@ -0,0 +1,21 @@
+// Name: muhammad faizan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0731
+// Section: A
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while(i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
+
+
---
+++ LAB 5/LAB WORK/lab 5 labwork 3.cpp
@@ -0,0 +1,24 @@
+// Name: muhammad faizan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0731
+// Section: A
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        cout << i << endl;
+        i++;
+    }
+    while(i <= 10);
+
+    return 0;
+}
+
+
+
+
